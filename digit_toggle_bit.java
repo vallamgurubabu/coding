812de515a3c2c6cc5780ecbb06ebca6e3382cc85
gurubabu vallam <vallamgurubabu@gmail.com>
2024-04-09 06:52:19 +0530

    commit 3

--- digit_toggle_bit.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class digit_toggle_bit {
-    public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
-        int n =s.nextInt();
-        
-    }
-}
