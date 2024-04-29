44f2817f21160c539c19ffec4c28b55b9bc001f6
gurubabu vallam <vallamgurubabu@gmail.com>
2024-04-29 22:30:56 +0530

    commit

--- fibonacci_series.java
+++ fibonacci_series.java
@@ -7,20 +7,26 @@ public class fibonacci_series {
         s.close();
         int a=0,b=1;
         int c=0;
+        int sum=1;
         if(n==1){
             System.out.println("The "+n+"th Fibonacc is 0");
+            sum=0;
         }else if(n==2){
             System.out.println("The "+n+"th Fibonacc is 1");
+            sum=1;
         }else{
             System.out.print(a+" "+b);
             for(int i=0;i<n-2;i++){
                 c=a+b;
+                sum+=c;
                 System.out.print(" "+c);
                 a=b;
                 b=c;
             }
             System.out.println("\nThe "+n+"th Fibonacc is "+c);
+            
         }
+        System.out.println("sum of  till "+n+"th fibonacci series "+ sum);
         
         
 
